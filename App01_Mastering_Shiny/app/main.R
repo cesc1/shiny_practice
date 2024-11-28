c0572b54fa6b2da13b49839481b2a79c2915b8cc
cesc1 <francesc.aguirre@gmail.com>
2024-11-28 03:59:56 +0100

    add the first example of the book, using box and rhino

--- App01_Mastering_Shiny/app/main.R
+++ App01_Mastering_Shiny/app/main.R
@@ -1,25 +1,33 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny[fluidPage, moduleServer, NS],
+  shiny[reactive, renderPrint, renderTable,
+        selectInput, tableOutput, verbatimTextOutput],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  fluidPage(
+    selectInput(ns("dataset"), label = "Dataset", choices = ls("package:datasets")),
+    verbatimTextOutput(ns("summary")),
+    tableOutput(ns("table"))
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
+    # Reactive expression
+    dataset <- reactive({
+      get(input$dataset, "package:datasets")
+    })
+
+    output$summary <- renderPrint({
+      summary(dataset())
+    })
+
+    output$table <- renderTable({
+      dataset()
     })
   })
 }
